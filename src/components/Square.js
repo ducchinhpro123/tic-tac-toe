ab7bd1fa8ae90d015356012ca91f022c7fc02b1f
Chin Com <duchinpro@gmail.com>
2023-08-26 20:40:27 +0700

    iteractive component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Square({ value }) {
-    return <button className='square'>{value}</button>
+export default function Square() {
+    const [value, setValue] = useState(null);
+
+    const handleClick = () => {
+        setValue("X")
+    }
+
+
+    return <button onClick={handleClick} className='square'>{value}</button>
 }
