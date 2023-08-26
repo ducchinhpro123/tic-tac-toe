ae3821e2578ab626f89bf867375023c9d030fa0f
Chin Com <duchinpro@gmail.com>
2023-08-26 20:35:24 +0700

    building the board

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-export default function Square() {
-    return (
-        <button className='square'>X</button>
-    )
+export default function Square({ value }) {
+    return <button className='square'>{value}</button>
 }
