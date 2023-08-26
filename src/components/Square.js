2aa193a5fa80c7e6b1de81969f73869b9f53a5ae
Chin Com <duchinpro@gmail.com>
2023-08-26 20:55:00 +0700

    completing the game

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 
-export default function Square() {
-    const [value, setValue] = useState(null);
+export default function Square({ value, onSquareClick }) {
+    // const [value, setValue] = useState(null);
 
-    const handleClick = () => {
-        setValue("X")
-    }
+    // const handleClick = () => {
+    //     setValue("X")
+    // }
 
 
-    return <button onClick={handleClick} className='square'>{value}</button>
+    return <button onClick={onSquareClick} className='square'>{value}</button>
 }
